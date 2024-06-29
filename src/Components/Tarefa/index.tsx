dbacbab7f09fa39dff4b963ca2bdbd8eebe1ef2c
Karen Aprigio Moraes <karenmoraes222@gmail.com>
2024-06-29 17:07:46 -0300

    criando o ambiente

--- src/Components/Tarefa/index.tsx
+++ src/Components/Tarefa/index.tsx
@@ -2,7 +2,14 @@ import * as S from './styles'
 
 const Tarefa = () => (
   <S.CardTarefa>
-    <S.Titulo>Nome tarefa</S.Titulo>
+    <S.Titulo>Nome da tarefa</S.Titulo>
+    <S.Tag>Importante</S.Tag>
+    <S.Tag>Pendente</S.Tag>
+    <S.Descricao />
+    <S.BarraAcoes>
+      <S.Botao>Edtar</S.Botao>
+      <S.Botao>Remover</S.Botao>
+    </S.BarraAcoes>
   </S.CardTarefa>
 )
 
--- src/Components/Tarefa/styles.ts
+++ src/Components/Tarefa/styles.ts
@@ -4,24 +4,38 @@ export const CardTarefa = styled.div`
   background-color: #fcfcfc;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   padding: 16px;
+  margin-bottom: 32px;
+  border-radius: 16px;
 `
 
 export const Titulo = styled.h3`
   font-weight: bold;
   font-size: 18px;
+  margin-bottom: 16px;
 `
 
 export const Tag = styled.span`
-  padding: 4px 8px;
+  display: inline-block;
+  padding: 4px 32px;
   font-size: 10px;
   font-weight: bold;
+  background-color: #1b9cfc;
+  margin-right: 16px;
+  border-radius: 8px;
 `
 
 export const Descricao = styled.textarea`
-  color: #8b8b8b;
+  display: block;
+  width: 100%;
+  margin-top: 16px;
+  color: #black;
   font-size: 14px;
   line-height: 24px;
   font-family: 'Roboto Mono', monospace;
+  margin-bottom: 16px;
+  resize: none;
+  border-radius: 16px;
+  background-color: #7ed6df;
 `
 
 export const BarraAcoes = styled.div`
@@ -36,4 +50,6 @@ export const Botao = styled.button`
   padding: 8px 12px;
   cursor: pointer;
   background-color: #2f3640;
+  border-radius: 8px;
+  margin-right: 8px;
 `
